d82c03edbc6aa6e1420b08986304ac11de577bd3
JimenaKatuMartinez <93katu@gmail.com>
2022-09-09 15:22:09 -0300

    vistas mobile y tabler de formulario y success

--- src/componentes/form/Form.js
+++ src/componentes/form/Form.js
@@ -25,7 +25,10 @@ const Form = () => {
 
   const setCardData = useCardUpdateContext();
 
-  const normalizedNumber = watch('cardholderNumber')?.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim();
+  const normalizedNumber = watch('cardholderNumber')
+    ?.replace(/[^\dA-Z]/g, '')
+    .replace(/(.{4})/g, '$1 ')
+    .trim();
   // Formatea el numero de la tarjeta para que tenga el siguinte patron: #### #### #### ####,
   // solo acepta números
 
@@ -41,97 +44,95 @@ const Form = () => {
   }, [watch()]);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Input
-        label={'CARDHOLDER NAME'}
-        type={'text'}
-        placeHolder={'e.g. Jane Appleseed'}
-        id={'input-name'}
-        hookForm={{
-          ...register('cardholderName', {
-            required: { value: true, message: "Can't be blank" },
-          }),
-        }}
-        errorMessaje={errors?.cardholderName?.message}
-      />
-      <Input
-        label={'CARDHOLDER NUMBER'}
-        type={'text'}
-        placeHolder={'e.g. 1234 4566 7899 0000'}
-        id={'input-number'}
-        hookForm={{
-          ...register('cardholderNumber', {
-            required: { value: true, message: "Can't be blank" },
-            pattern: {
-              value: /^[\d ]*$/,
-              message: 'Wrong formar, numbers only',
-            },
-            maxLength: {
-              value: 19,
-              message: 'Must contain a max of 16 digits.',
-            },
-            minLength: {
-              value: 18,
-              message: 'Must contain a min of 15 digits.',
-            },
-          }),
-        }}
-        errorMessaje={errors?.cardholderNumber?.message}
-        value={normalizedNumber}
-        maxLength={19}
-      />
-      <InputGroup
-        hookFormMM={{
-          ...register('cardholderMM', {
-            required: { value: true, message: "Can't be blank" },
-            min: {
-              value: 2,
-              message: 'Must contain 2 digits.',
-            },
-            pattern: {
-              value: /^[0-9]*$/,
-              message: 'Wrong formar, numbers only',
-            },
-          }),
-        }}
-
-        hookFormYY={{
-          ...register('cardholderYY', {
-            required: { value: true, message: "Can't be blank" },
-            min: {
-              value: 2,
-              message: 'Must contain 2 digits.',
-            },
-            pattern: {
-              value: /^[0-9]*$/,
-              message: 'Wrong formar, numbers only',
-            },
-          }),
-        }}
-
-        hookFormCVC={{
-          ...register('cardholderCVC', {
-            required: { value: true, message: "Can't be blank" },
-            minLength: {
-              value: 3,
-              message: 'Must contain a min of 3 digits.',
-            },
-            maxLength: {
-              value: 4,
-              message: 'Must contain a max of 4 digits.',
-            },
-            pattern: {
-              value: /^[0-9]*$/,
-              message: 'Wrong formar, numbers only',
-            },
-          }),
-        }}
-        errors={errors}
-        errorMessajeMM={errors?.cardholderMM?.message}
-        errorMessajeYY={errors?.cardholderYY?.message}
-        errorMessajeCVC={errors?.cardholderCVC?.message}
-      />
-      <Button text={'Confirm'} type={'submit'}/>
+    <form className='wrapper-form' onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          label={'CARDHOLDER NAME'}
+          type={'text'}
+          placeHolder={'e.g. Jane Appleseed'}
+          id={'input-name'}
+          hookForm={{
+            ...register('cardholderName', {
+              required: { value: true, message: "Can't be blank" },
+            }),
+          }}
+          errorMessaje={errors?.cardholderName?.message}
+        />
+        <Input
+          label={'CARDHOLDER NUMBER'}
+          type={'text'}
+          placeHolder={'e.g. 1234 4566 7899 0000'}
+          id={'input-number'}
+          hookForm={{
+            ...register('cardholderNumber', {
+              required: { value: true, message: "Can't be blank" },
+              pattern: {
+                value: /^[\d ]*$/,
+                message: 'Wrong formar, numbers only',
+              },
+              maxLength: {
+                value: 19,
+                message: 'Must contain a max of 16 digits.',
+              },
+              minLength: {
+                value: 18,
+                message: 'Must contain a min of 15 digits.',
+              },
+            }),
+          }}
+          errorMessaje={errors?.cardholderNumber?.message}
+          value={normalizedNumber}
+          maxLength={19}
+        />
+        <InputGroup
+          hookFormMM={{
+            ...register('cardholderMM', {
+              required: { value: true, message: "Can't be blank" },
+              min: {
+                value: 2,
+                message: 'Must contain 2 digits.',
+              },
+              pattern: {
+                value: /^[0-9]*$/,
+                message: 'Wrong formar, numbers only',
+              },
+            }),
+          }}
+          hookFormYY={{
+            ...register('cardholderYY', {
+              required: { value: true, message: "Can't be blank" },
+              min: {
+                value: 2,
+                message: 'Must contain 2 digits.',
+              },
+              pattern: {
+                value: /^[0-9]*$/,
+                message: 'Wrong formar, numbers only',
+              },
+            }),
+          }}
+          hookFormCVC={{
+            ...register('cardholderCVC', {
+              required: { value: true, message: "Can't be blank" },
+              minLength: {
+                value: 3,
+                message: 'Must contain a min of 3 digits.',
+              },
+              maxLength: {
+                value: 4,
+                message: 'Must contain a max of 4 digits.',
+              },
+              pattern: {
+                value: /^[0-9]*$/,
+                message: 'Wrong formar, numbers only',
+              },
+            }),
+          }}
+          errors={errors}
+          errorMessajeMM={errors?.cardholderMM?.message}
+          errorMessajeYY={errors?.cardholderYY?.message}
+          errorMessajeCVC={errors?.cardholderCVC?.message}
+        />
+      <Button text={'Confirm'} type={'submit'} />
     </form>
   );
 };
